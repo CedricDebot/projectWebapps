d8b8b1f969313843a5757a96f6db1a54f1bf85b5
Cedric  Debot <cedricdebot@MacBook-Pro-van-Cedric.local>
2016-11-16 10:58:22 +0100

    added login and register page

--- src/js/auth/auth.controller.js
+++ src/js/auth/auth.controller.js
@@ -6,12 +6,13 @@ class AuthController {
     this.title = $state.current.title;
     this._User = User;
     this._$state = $state;
+    this.authType = $state.current.name.replace('app.', '');
   }
 
   submitForm() {
     this.isSubmitting = true;
 
-    this._User.attemptAuth(this.formData).then(
+    this._User.attemptAuth(this.authType, this.formData).then(
       (res) => {
         this._$state.go('app.home');
       },
